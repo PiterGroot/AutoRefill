be01e9c57619907e4e81eae098240cd7acb7b6f1
PiterGroot <piter.groot1104@gmail.com>
2024-04-21 22:34:42 +0200

    Started working on block placement detection

--- src/main/java/pitaah/examplemod/ExampleMod.java
+++ src/main/java/pitaah/examplemod/ExampleMod.java
@@ -1,14 +1,11 @@
 package pitaah.examplemod;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.entity.player.EntityPlayer;
-import net.minecraft.core.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
+
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
-
 
 public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "examplemod";
@@ -20,8 +17,7 @@ public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEn
 
 	@Override
 	public void beforeGameStart() {
-
-
+		LOGGER.info("Loaded in the game!");
 	}
 
 	@Override
