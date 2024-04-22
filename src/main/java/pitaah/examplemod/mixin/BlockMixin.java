31f77b976896cc8ab1f445628124f0f5cb10040b
PiterGroot <piter.groot1104@gmail.com>
2024-04-22 21:46:49 +0200

    Added automatic slot refill if possible for most common block types

---
+++ src/main/java/pitaah/examplemod/mixin/BlockMixin.java
@@ -0,0 +1,22 @@
+package pitaah.examplemod.mixin;
+import pitaah.examplemod.ExampleMod;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.core.entity.EntityLiving;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.world.World;
+
+@Mixin(value = Block.class, remap = false)
+public abstract class BlockMixin
+{
+	@Inject(method = "onBlockPlaced", at = @At("HEAD"))
+	public void OnBlockAxisAlignedPlaced(World world, int x, int y, int z, Side side, EntityLiving entity, double sideHeight, CallbackInfo ci)
+	{
+		ExampleMod.OnBlockPlaced(entity, false);
+	}
+}
